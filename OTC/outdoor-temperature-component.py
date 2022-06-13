1c6ec61a89cfd14a20a59bf1f407a7262e2a1484
Luca Bongiovanni <lucabongio97@gmail.com>
2022-06-13 12:34:15 +0200

    :bug: Fix timezone bug in OTC

--- OTC/outdoor-temperature-component.py
+++ OTC/outdoor-temperature-component.py
@@ -2,8 +2,11 @@ import os
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
 import datetime
-from meteostat import Point, Daily
+from meteostat import Point, Hourly
 import pandas as pd
+import schedule
+import time
+from datetime import timezone
 
 # influx parameters
 if "INFLUX_WRITE_BUCKET" in os.environ:
@@ -29,8 +32,17 @@ else:
 if "INFLUX_URL" in os.environ:
     influx_url = os.environ["INFLUX_URL"]
 else:
-    influx_url = "http://influxdb:8086"
+    influx_url = "http://localhost:8086"
 
+if "MEASUREMENT" in os.environ:
+    measurement = os.environ["MEASUREMENT"]
+else:
+    measurement = "IoT-Device"
+
+if "DEVICEID" in os.environ:
+    device_id = os.environ["DEVICEID"]
+else:
+    device_id = 1
 
 print("Attempting to connect to: %s\nOrg: %s\nBucket: %s" %
       (influx_url, org, read_bucket))
@@ -45,73 +57,64 @@ write_api = influx_client.write_api(write_options=SYNCHRONOUS)
 # Class that contains GPS values and first and last timestamps of Influx data
 
 
-class InfluxInfo:
-    def __init__(self, device_id):
-        super().__init__()
-        query = ' from(bucket:"{}")\
-                    |> range(start: -30d, stop: now())\
+def get_GPS() -> str:
+    query = ' from(bucket:"{}")\
+                    |> range(start: 0)\
                     |> filter(fn:(r) => r.device_id == "{}")\
-                    |> first() '.format(read_bucket, device_id)
-        result = query_api.query(org=org, query=query)
-        for table in result:
-            for record in table.records:
-                gps = record.values.get("GPS")
-                self.first_dt = record.get_time()
-        query = ' from(bucket:"{}")\
-                    |> range(start: -30d, stop: now())\
-                    |> filter(fn:(r) => r.device_id == "{}")\
-                    |> last() '.format(read_bucket, device_id)
-        result = query_api.query(org=org, query=query)
-        for table in result:
-            for record in table.records:
-                self.last_dt = record.get_time()
+                    |> filter(fn:(r) => r._measurement == "{}")\
+                    |> last() '.format(read_bucket, device_id, measurement)
+    result = query_api.query(org=org, query=query)
+    for table in result:
+        for record in table.records:
+            gps = record.values.get("GPS")
 
-        print("InfluxInfo initialized! Found\nGPS: %s\nFirst DateTime: %s\nLast DateTime: %s" % (
-            gps, self.first_dt, self.last_dt))
+    print("Got \nGPS: %s\n" % gps)
+    return gps
 
-        gps_list = gps.split(",")
-        self.lat = float(gps_list[0])
-        self.long = float(gps_list[1])
 
-    def _getLat(self) -> float:
-        return self.lat
-
-    def _getLong(self) -> float:
-        return self.long
-
-    def _getFirstDT(self) -> datetime.date:
-        return datetime.datetime(self.first_dt.year, self.first_dt.month, self.first_dt.day)
-
-    def _getLastDT(self) -> datetime.date:
-        return datetime.datetime(self.last_dt.year, self.last_dt.month, self.last_dt.day)
+print("Gathering gps from influxdb bucket: %s" % read_bucket)
+gps = get_GPS()
 
 # Function to send the otc fetched data to influxdb bucket
 
 
 def send_otc_data(data):
+    tosend = pd.DataFrame()
+    tosend["temp"] = data["temp"]
     data.reset_index(inplace=True)
-    data["_time"] = pd.to_datetime(data["time"])
-    data.set_index("_time", inplace=True)
+    tosend["_time"] = data["time"].values.astype('<M8[s]')
 
-    data.drop(columns=["time", "tmin", "tmax", "prcp", "snow",
-              "wdir", "wspd", "wpgt", "pres", "tsun"], inplace=True)
+    tosend.set_index("_time", inplace=True)
 
-    write_api.write(bucket=write_bucket, org=org, record=data,
-                    data_frame_measurement_name="otc")
+    write_api.write(bucket=write_bucket, org=org, record=tosend,
+                    data_frame_measurement_name="otc2")
     print("Successfully sent data to influxdb!")
 
 
-def main():
-    print("Gathering info from influxdb bucket: %s" % read_bucket)
-    info = InfluxInfo(1)
+def fetch_and_send():
+    gps_list = gps.split(",")
+    latitude = float(gps_list[0])
+    longitude = float(gps_list[1])
 
-    # Getting daily values for the chosen locality from 15 days before the first gathered data
-    print("Getting outdoor temperature value through meteostat")
-    data = Daily(Point(info._getLat(), info._getLong()),
-                 info._getFirstDT() - datetime.timedelta(15), info._getLastDT()).fetch()
-
+    # Getting last hour hourly values for the chosen locality
+    print("Getting last hour outdoor temperature value through meteostat")
+    data = Hourly(Point(latitude, longitude),
+                  datetime.datetime.now() - datetime.timedelta(hours=1), datetime.datetime.now(), timezone="Europe/Rome").fetch()
     print("Sending fetched data to influxdb")
+    print(data)
     send_otc_data(data)
+
+
+schedule.every().hour.do(fetch_and_send)
+
+
+def main():
+
+    fetch_and_send()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 
 if __name__ == "__main__":
